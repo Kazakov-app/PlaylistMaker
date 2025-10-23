a1c116f41360b9303d4855ab79ce127dc0d0ace5
Kazakov-app <kazakov.app@yandex.ru>
2025-10-24 00:31:24 +0300

    fix notice

--- app/src/main/java/com/example/playlistmaker/main/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/main/MainActivity.kt
@@ -4,6 +4,9 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.playlistmaker.R
@@ -15,6 +18,12 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_container)) { view, insets ->
+            val statusBar = insets.getInsets(WindowInsetsCompat.Type.statusBars())
+            val navigationBars = insets.getInsets(WindowInsetsCompat.Type.navigationBars())
+            view.updatePadding(top = statusBar.top, bottom = navigationBars.bottom)
+            insets
+        }
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
